14565417d72181136b84b0cdf20ef37c34dcc4fa
Marie Baude <mariebaude@maries-mbp.home>
2024-12-11 12:05:42 +0100

    init project front and back

---
+++ back/src/test/java/com/example/MPMT/MpmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MPMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
